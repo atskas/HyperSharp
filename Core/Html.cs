717bf435d742f43de7bbca18774b7ea0606febf3
Pan <atskas0@gmail.com>
2025-05-30 00:38:37 +0300

    Add indentation helper method and utilise it in current html elements

--- Core/Html.cs
+++ Core/Html.cs
@@ -5,22 +5,33 @@ public class Html
     private HtmlBuilder builder = new HtmlBuilder();
     private HtmlCompiler compiler = new HtmlCompiler();
     
+    // Helper method for indentation
+    // helps keep HTML clean
+    private int indentLevel = 0;
+    private string Indent() => new string(' ', indentLevel * 2);
+    
+    #region Html Elements
+    
+    public string DOCTYPE() => "<!DOCTYPE html>\n";
+    
     public void Div(Action innerContent)
     {
-        builder.Append("<div>");
+        builder.Append($"{Indent()}<div>\n");
+        indentLevel++;
         innerContent();
-        builder.Append("</div>");
+        indentLevel--;
+        builder.Append("</div>\n");
     }
 
     public void Span(string innerContent)
     {
-        builder.Append("<span>");
-        builder.Append(innerContent);
-        builder.Append("</span>");
+        builder.Append($"{Indent()}<span>{innerContent}</span>\n");;
     }
     
+    #endregion
     public string GetHtml() => builder.ToString();
     public void Compile(string output) => compiler.Compile(output);
 
     public void SetOutputPath(string path) => compiler.OutputPath = path;
+    
 }
